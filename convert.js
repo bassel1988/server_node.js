f343dbf73fea8ba962080b345b028f23d8335b42
bassel1988 <bassel.t88h@gmail.com>
2025-04-29 13:01:25 +0300

    Add files via upload

--- convert.js
+++ convert.js
@@ -3,9 +3,12 @@ const fs = require('fs');
 // اقرأ ملف service-account.json
 const serviceAccount = require('./service-account.json');
 
+// إصلاح مشكلة newlines في المفتاح الخاص
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 // حوّل البيانات إلى نص مشفّر (JSON string)
 const stringified = JSON.stringify(serviceAccount);
 
-// اطبع الناتج
-console.log("\n⭐ انسخ هذا النص واستعمله في Environment Variables:");
+// اطبع الناتج المعدل
+console.log("\n⭐ انسخ هذا النص المعدل واستعمله في Environment Variables:");
 console.log(stringified);
